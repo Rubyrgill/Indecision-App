c5e23afba73c5681a23fcd8bcbc7ec2748d343fb
RubyG <rubyrgill@hotmail.com>
2018-12-30 13:32:00 -0500

    Using babel to compile

--- Application/public/scripts/app.js
+++ Application/public/scripts/app.js
@@ -2,7 +2,12 @@ console.log("App.js is running")
 
 
 //JSX - JavaScript XML (JS Syntax Extenstion)
-var template = <p>This is JSX from app.js</p>; //Static
+//var template = <p id="testid">This is JSX from app.js</p>; 
+var template = React.createElement(
+    "p",
+    { id: "testid" },
+    "This is JSX from app.js"
+);
 var appRoot = document.getElementById("app")
 
 //Takes 2 arguments 
