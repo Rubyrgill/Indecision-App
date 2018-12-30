924395b87aaea30f7403c38baee235c9e9437920
RubyG <rubyrgill@hotmail.com>
2018-12-30 14:32:23 -0500

    Testing template two - activity

--- Application/public/scripts/app.js
+++ Application/public/scripts/app.js
@@ -6,14 +6,45 @@ console.log("App.js is running");
 
 //JSX - JavaScript XML (JS Syntax Extenstion)
 var template = React.createElement(
-  "h1",
-  { id: "testid" },
-  "Indecision App"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        { id: "apptitle" },
+        "Indecision App"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Information about the app"
+    )
 );
 
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Ruby Gill"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: 22"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: Canada"
+    )
+);
 var appRoot = document.getElementById("app");
 
 //Takes 2 arguments 
 //(1)-JSX what you want to render 
 //(2) DOM element - where you want to render it
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
+
+//running in server to watch for changes 
+//babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
--- Application/src/app.js
+++ Application/src/app.js
@@ -4,11 +4,26 @@ console.log("App.js is running")
 
 
 //JSX - JavaScript XML (JS Syntax Extenstion)
-var template = <h1 id="testid">Indecision App</h1>;
+var template = (
+    <div>
+        <h1 id="apptitle">Indecision App</h1>
+        <p>Information about the app</p>
+    </div>
+);
 
+var templateTwo = (
+    <div>
+        <h1>Ruby Gill</h1>
+        <p>Age: 22</p>
+        <p>Location: Canada</p>
+    </div>
+);
 var appRoot = document.getElementById("app")
 
 //Takes 2 arguments 
 //(1)-JSX what you want to render 
 //(2) DOM element - where you want to render it
-ReactDOM.render(template, appRoot)
+ReactDOM.render(templateTwo, appRoot)
+
+//running in server to watch for changes 
+//babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
